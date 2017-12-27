dc349ea934aa48454ce449058e625209fc378b0e
Sourabh Shirhatti <shirhatti@outlook.com>
2017-12-26 23:09:48 -0600

    Add Console App
    Rename Console -> ConsoleApp

---
+++ src/AdventOfCode.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AdventOfCode;
+
+namespace AdventOfCode.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
